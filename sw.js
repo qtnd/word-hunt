65b3bd8a168ab3b8d4366b00d239f895f1165b71
qtnd <124354328+qtnd@users.noreply.github.com>
2025-07-04 17:49:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,19 @@
-const CACHE_NAME = "mots-caches-v1";
+const CACHE_NAME = 'wordhunt-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./style.css",
-  "./script.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './manifest.json',
+  // Ajoutez vos fichiers css/js/images ici
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
